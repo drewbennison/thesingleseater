82bbca47081ef6e0fe89617b2896b17d7d5434d4
drewbennison <46604140+drewbennison@users.noreply.github.com>
2020-03-31 23:43:03 -0400

    csv from github now

--- scripts/elo.R
+++ scripts/elo.R
@@ -1,8 +1,9 @@
 library(data.table)
 library(tidyverse)
+library(lubridate)
 
-dt <-fread("C:/Users/drewb/Desktop/thesingleseater/datasets/master_backup/indycar_results.csv")
-sp_elo <- fread("C:/Users/drewb/Desktop/thesingleseater/datasets/starting_position_elo.csv")
+dt <- fread("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/master_backup/indycar_results.csv")
+sp_elo <- fread("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/starting_position_elo.csv")
 
 #Initialize elo ratings, k
 elo_ratings_initial <- dt %>% select(driver) %>% unique() %>% mutate(EloRating=1500)
@@ -13,7 +14,8 @@ tracker <- tibble(driver=elo_ratings_initial$driver, date=ymd("2021-01-01"), yea
 k_optimization <- tibble(errorXWin=0, errorXWin2=0) 
 
 #Select variables we want
-dt <- dt %>% select(year, raceNumber, driver, fin, st, date)
+dt <- dt %>% select(year, raceNumber, driver, fin, st, date) %>% 
+  mutate(date=ymd(date))
 
 #Starting and ending year range
 yr <- c(2008:2019)
@@ -49,20 +51,20 @@ for(i in 1:max(current_year$raceNumber)) {
     tracker <- add_row(tracker, driver=elo$x[j], date=elo$date[j], year=a, EloRating=elo$newRating[j])
   }
   
-  k_optimization <- add_row(k_optimization, errorXWin = current_race_cross$xWin, errorXWin2=current_race_cross$XexpectedWin)
+  #k_optimization <- add_row(k_optimization, errorXWin = current_race_cross$xWin, errorXWin2=current_race_cross$XexpectedWin)
   }
 }
 
 #Calculates the brier score
-k_optimization %>% mutate(error=(errorXWin-errorXWin2)^2) %>% 
-  summarise(e=mean(error))
+#k_optimization %>% mutate(error=(errorXWin-errorXWin2)^2) %>% 
+#  summarise(e=mean(error))
 
-#elo rating by races since 2008
+#  elo rating by races since 2008  ###############################################
 tracker %>% filter(date!="2021-01-01", driver %in% c("Josef Newgarden", "Simon Pagenaud","Scott Dixon",
                                  "Marco Andretti", "Alexander Rossi")) %>%
   ggplot(aes(x=date, y=EloRating, color=driver)) + geom_line()
 
-#elo rating by race number in career
+#  elo rating by race number in career  #########################################
 tracker %>%
   filter(date!="2021-01-01") %>% 
   group_by(driver) %>%
@@ -73,7 +75,7 @@ tracker %>%
   labs(x="Race number in career starting in 2008")
 
 ##################################################################
-#Probability of finishing in first place in a single race
+#  Probability of finishing in first place in a single race ######
 r<-tibble(driver="test",winprob=0)
 hypothetical_race <- elo_ratings %>% filter(EloRating>1570)
 for(i in (1:nrow(hypothetical_race))) {
