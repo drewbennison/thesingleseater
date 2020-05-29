bc912bf99fce13f1360947267ffeb4bfb3971bc9
drewbennison <46604140+drewbennison@users.noreply.github.com>
2020-05-29 11:38:54 -0400

    new and improved

--- scripts/raceStats.py
+++ scripts/raceStats.py
@@ -1,6 +1,4 @@
 #TO DO:
-#Sort by race results (probably have to bring in a file)
-#Map my names (rhr) to Racing Reference names for easier merging later on
 
 from pandas import DataFrame
 import pandas as pd
@@ -14,73 +12,62 @@ print("Function greenFlagStats(list_window=[startWindow, endWindow])")
 user_input = str(input("lapchart_link source: "))
 data = pd.read_csv(user_input)
 
+driver_input = str(input("drivers in race source: "))
+driver_input_data = pd.read_csv(driver_input)
+
 ##################################################################################################
 #Finds every drivers' position on every lap in the given interval
-names = ["leist", "kanaan", "veach", "rossi", "rhr", "andretti", "hinchcliffe", "ericsson", "chilton", "dixon", "rosenqvist", "ferrucci", "bourdais", "pigot", "herta", "rahal", "sato", "newgarden", "power", "pagenaud", "castroneves", "harvey", "hanley", "carpjones", "kimballoward", "oward", "karam", "daly", "kaiser", "mann", "king", "alonso", "jonesindy"]
 
 def racePosition(start=0, finish=1):
 
-		#Full time drivers
-		leist = []
-		kanaan = []
-		veach = []
-		rossi = []
-		rhr = []
-		andretti = []
-		hinchcliffe = []
-		ericsson = []
-		chilton = []
-		dixon = []
-		rosenqvist = []
-		ferrucci = []
-		bourdais = []
-		pigot = []
-		herta = []
-		rahal = []
-		sato = []
-		newgarden = []
-		power = []
-		pagenaud = []
-		#Part time drivers
-		castroneves = [] #5,6
-		harvey = [] #1–6, 10, 13, 16, 17
-		hanley = [] # 1, 3, 6, 10, 13
-		carpjones = [] #alternate road and oval
-		kimballoward = [] #not positive yet
-		oward = []
-		karam = [] #6
-		daly = [] #6
-		kaiser = [] #2
-		mann = [] #6
-		king = [] #6
-		alonso = [] #6
-		jonesindy = [] #indy number only
+	names = []
 
-		#Driver numbers in order they are listed above
-		nums = [4.0, 14.0, 26.0, 27.0, 28.0, 98.0, 5.0, 7.0, 59.0, 9.0, 10.0, 19.0, 18.0, 21.0, 88.0, 15.0, 30.0, 2.0, 12.0, 22.0, 3.0, 60.0, 81.0, 20.0, 23.0, 31.0, 24.0, 25.0, 32.0, 39.0, 42.0, 66.0, 64.0]
-		#List of all driver lists in order of numbers above
-		drivers = [leist, kanaan, veach, rossi, rhr, andretti, hinchcliffe, ericsson, chilton, dixon, rosenqvist, ferrucci, bourdais, pigot, herta, rahal, sato, newgarden, power, pagenaud, castroneves, harvey, hanley, carpjones, kimballoward, oward, karam, daly, kaiser, mann, king, alonso, jonesindy]
-		#Driver names in order
-		names = ["leist", "kanaan", "veach", "rossi", "rhr", "andretti", "hinchcliffe", "ericsson", "chilton", "dixon", "rosenqvist", "ferrucci", "bourdais", "pigot", "herta", "rahal", "sato", "newgarden", "power", "pagenaud", "castroneves", "harvey", "hanley", "carpjones", "kimballoward", "oward", "karam", "daly", "kaiser", "mann", "king", "alonso", "jonesindy"]
+	for i in range(0,len(driver_input_data)):
+		names.append(driver_input_data.iloc[i,0])
+	
+	driver_dict = {k:[] for k in names}
+	#print(driver_dict)
 
-		#Range of 0 (start) to (last lap+1) of race
-		for lap in range(start,finish):
-		    #Take column of current lap into a list
-		    place_list = list(data[str(lap)])
-		    
-		    count = 0
-		    #For each number in nums
-		    for num in nums:
-		        #If the number was on the track for that lap
-		        if num in place_list:
-		            #Find it's index in the list, and append it to the correct driver
-		            driver = drivers[count]
-		            place = place_list.index(num)
-		            driver.append(place+1)
-		        else:
-		            pass
-		        count += 1
-		return drivers
+	#Driver numbers in order they are listed above
+	nums = []
+	for i in range(0,len(driver_input_data)):
+		nums.append(int(driver_input_data.iloc[i,1]))
+
+	#List of all driver lists in order of numbers above
+	
+	def getList(dict): 
+		list = [] 
+		for key in dict.keys(): 
+			list.append(key)
+		return list
+      
+	drivers = getList(driver_dict)
+
+	#Driver names in order - not sure if this is still needed
+	names = []
+	for i in range(0,len(driver_input_data)):
+		names.append(driver_input_data.iloc[i,0])
+
+	#Range of 0 (start) to (last lap+1) of race
+	for lap in range(start,finish):
+	    #Take column of current lap into a list
+	    place_list = list(data[str(lap)])
+	    
+	    count = 0
+	    #For each number in nums
+	    for num in nums:
+	        #If the number was on the track for that lap
+	        if num in place_list:
+	            #Find it's index in the list, and append it to the correct driver
+	            driver = drivers[count]
+	            place = place_list.index(num)
+	            print(driver, place)
+	            driver_dict[driver].append(place+1)
+	        else:
+	            pass
+	        count += 1
+
+	return driver_dict 
 
 ##################################################################################################
 #Calculates all of the race stats for a given race
@@ -101,36 +88,30 @@ def raceStats(race_length, point_system="single"):
 	#ATP
 	df = pd.DataFrame({"driver":[], "atp":[]})
 
-	count = 0
-	for driver in drivers:
-	    if len(driver) != 0:
-	        df2 = pd.DataFrame({"driver":[names[count]], "atp":[sum(driver)/len(driver)]})
+	for key in drivers:
+	    if len(drivers[key]) != 0:
+	        df2 = pd.DataFrame({"driver":[key], "atp":[sum(drivers[key])/len(drivers[key])]})
 	        df = df.append(df2, ignore_index=True)
-	    count += 1
 
 	##################################################################################################
 	#Did they lead a lap? The most laps? Get pole position?
 	laps_led = pd.DataFrame({"driver":[], "ledLaps":[]})
 
-	count = 0
-	for driver in drivers:
-		if len(driver) != 0:
+	for key in drivers:
+		if len(drivers[key]) != 0:
 	 		led_laps = 0
-	 		for i in range(1,len(driver)):
-	 			if driver[i] == 1:
+	 		for i in range(0,len(drivers[key])):
+	 			if drivers[key][i] == 1:
 	 				led_laps += 1
-	 		df2=pd.DataFrame({"driver":[names[count]], "ledLaps":[led_laps]})
+	 		df2=pd.DataFrame({"driver":[key], "ledLaps":[led_laps]})
 	 		laps_led = laps_led.append(df2, ignore_index=True)
-		count+=1
 
 	#Check who got pole position
-	count = 0
 	pole = 0
-	for driver in drivers:
-		if len(driver) !=0:
-			if driver[0] == 1:
-				pole = names[count]
-		count += 1
+	for key in drivers:
+		if len(drivers[key]) !=0:
+			if drivers[key][0] == 1:
+				pole = key
 
 
 	laps_led['ledLapPoints'] = np.where(laps_led['ledLaps']>0, 1, 0)
@@ -138,16 +119,16 @@ def raceStats(race_length, point_system="single"):
 	laps_led['ledMostPoints'] = np.where(laps_led['ledLaps']==most_laps,2,0)
 	laps_led['polePoints'] = np.where(laps_led['driver']==pole,1,0)
 
+
 	##################################################################################################
 	#Number of passes measured at the end of each lap
 	passes_df = pd.DataFrame({"driver":[], "passesFor":[], "passesAgainst":[], "totalPasses":[]})
 
-	count = 0
-	for driver in drivers:
-	    if len(driver) != 0:
+	for key in drivers:
+	    if len(drivers[key]) != 0:
 	        changes = []
-	        for i in range(0, len(driver)-1):
-	            change=-(driver[i+1]-driver[i])
+	        for i in range(0, len(drivers[key])-1):
+	            change=-(drivers[key][i+1]-drivers[key][i])
 	            changes.append(change)
 
 	    goodpass = []
@@ -158,37 +139,31 @@ def raceStats(race_length, point_system="single"):
 	        else:
 	            badpass.append(i)
 	    
-	    if len(driver) != 0:
-	        df2 = pd.DataFrame({"driver":[names[count]], "passesFor":[sum(goodpass)], "passesAgainst":[-1*sum(badpass)], "totalPasses":[sum(goodpass)+sum(map(abs, badpass))]})
+	    if len(drivers[key]) != 0:
+	        df2 = pd.DataFrame({"driver":[key], "passesFor":[sum(goodpass)], "passesAgainst":[-1*sum(badpass)], "totalPasses":[sum(goodpass)+sum(map(abs, badpass))]})
 	        passes_df = passes_df.append(df2, ignore_index=True)
-	    count +=1
 	    
 	##################################################################################################
 	#In Top Five Laps
 	in_top_five_df = pd.DataFrame({"driver":[], "lapsT5":[]})
 
-	count = 0
-	for driver in drivers:
+	for key in drivers:
 	    inT5 = 0
-	    if len(driver) !=0:
-	        for lap in driver:
+	    if len(drivers[key]) !=0:
+	        for lap in drivers[key]:
 	            if lap <6:
 	                inT5+=1
-	    if inT5>0:
-	        df2 = pd.DataFrame({"driver":[names[count]], "lapsT5":[inT5-1]})
-	        in_top_five_df = in_top_five_df.append(df2, ignore_index=True)
-	    count +=1
+	    df2 = pd.DataFrame({"driver":[key], "lapsT5":[inT5]})
+	    in_top_five_df = in_top_five_df.append(df2, ignore_index=True)
 
 	##################################################################################################
 	#Race start change
 	start_df = pd.DataFrame({"driver":[], "lapOneChange":[]})
 
-	count = 0
-	for driver in drivers:
-	    if len(driver) >=2:
-	        df2 = pd.DataFrame({"driver":[names[count]], "lapOneChange":[-1*(driver[2]-driver[0])]})
+	for key in drivers:
+	    if len(drivers[key]) >=2:
+	        df2 = pd.DataFrame({"driver":[key], "lapOneChange":[-1*(drivers[key][2]-drivers[key][0])]})
 	        start_df = start_df.append(df2, ignore_index=True)
-	    count += 1
 
 	##################################################################################################
 	#ATP25
@@ -197,9 +172,9 @@ def raceStats(race_length, point_system="single"):
 	atp_percentile = pd.DataFrame({"driver":[], "atp25":[]})
 
 	count = 0
-	for driver in drivers:
-	    if len(driver) != 0:
-	        df2 = pd.DataFrame({"driver":[names[count]], "atp25":[sum(driver)/len(driver)]})
+	for key in drivers:
+	    if len(drivers[key]) != 0:
+	        df2 = pd.DataFrame({"driver":[key], "atp25":[sum(drivers[key])/len(drivers[key])]})
 	        atp_percentile = atp_percentile.append(df2, ignore_index=True)
 	    count += 1
 
@@ -219,14 +194,16 @@ def raceStats(race_length, point_system="single"):
 	df['roundedATP25'] = round(df['atp25'])
 	df = df.merge(points, how="left", left_on="roundedATP", right_on="fin")
 	df = df.merge(points, how="left", left_on="roundedATP25", right_on="fin")
-	df['xPoints'] = df['ledLapPoints']+df['ledMostPoints']+df['polePoints']+.75*df['points_x']+.25*df['points_y']
+	df['xPoints'] = df['ledLapPoints']+df['ledMostPoints']+df['polePoints']+1*df['points_x']
 
 	##################################################################################################
 	#Final output
 	final = df[['driver', 'ledLapPoints', 'ledMostPoints','polePoints','atp', 'atp25', 'points_x','points_y','xPoints','passesFor','passesAgainst','lapOneChange','lapsT5']]
+	final.to_csv("advanced_race_stats.csv")
 	return final
 
 
+'''
 def cautionStats(list_of_cautions):
 	#Takes a list as an input for cautions
 
@@ -259,3 +236,5 @@ def greenFlagStats(list_window):
 			greenFlag_df = greenFlag_df.append(df2, ignore_index=True)
 		count+=1
 	return greenFlag_df
+
+'''
