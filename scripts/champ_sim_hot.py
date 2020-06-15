c0d03b47a567a50495e96874f3db983b050abd8f
drewbennison <46604140+drewbennison@users.noreply.github.com>
2020-06-15 08:59:01 -0400

    Only drivers in each race are simulated

--- scripts/champ_sim_hot.py
+++ scripts/champ_sim_hot.py
@@ -8,40 +8,54 @@ pd.set_option('display.max_columns', 500)
 '''
 Needed updates:
 - update number of races left each time and double points races
-- hot elo ratings update after each race
 '''
 points_table = pd.read_csv("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/points_table.csv")
 
-
-
 final_results = pd.DataFrame(data={'driver': ['test'], 'totalPoints': [0], 'chamPos': [0], 'season': [0]})
 
-# read in master stats, select top n drivers by current points
+# read in master stats for driver points
 season_drivers_file = pd.read_csv("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/master_backup/indycar_results.csv", encoding = "ISO-8859-1")
 season_drivers_file = season_drivers_file[season_drivers_file.year == 2020]
 season_drivers_file = season_drivers_file.groupby('driver')['pts'].agg(points='sum')
 season_drivers_file = season_drivers_file.reset_index(level=['driver'])
 
+# read in drivers and the races they are competing in this season
+drivers_and_races = pd.read_csv("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/indycar_2020_drivers_races.csv", encoding = "ISO-8859-1")
+drivers_and_races = drivers_and_races.merge(season_drivers_file, how='left', left_on="Driver", right_on="driver")
+drivers_and_races = drivers_and_races.fillna(0)
+drivers_and_races['driver'] = drivers_and_races['Driver']
+drivers_and_races = drivers_and_races[['driver', 'Rounds', 'points']]
+
+# elo ratings table file
+elo_ratings_file = pd.read_csv("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/elo_ratings/elo_tracker.csv")
+
 # for each season, create a season race results table
-for season in range(1, 1001):
+for season in range(1, 4001):
     print(season)
     startTime = datetime.now()
     race_results = pd.DataFrame(data={'season': [0], 'race': [0], 'driver': ["test"], 'fin': [0]})
     # Must be in form driver, points
-    season_drivers = season_drivers_file
+    season_drivers = drivers_and_races
 
     # The current elo ratings for all drivers
-    elo_ratings_table = pd.read_csv("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/elo_ratings/elo_tracker.csv")
+    elo_ratings_table = elo_ratings_file
     elo_ratings_table = elo_ratings_table[elo_ratings_table.year != 2000]
     elo_ratings_table = elo_ratings_table[elo_ratings_table.groupby('driver').date.transform('max') == elo_ratings_table['date']]
     elo_ratings_table = elo_ratings_table[['driver', 'EloRating']]
 
     # for each race of that season
-    for race in range(1, 14):
-        #print("race start elo")
-        #print(elo_ratings_table)
+    for race in range(2, 15):
+        # season_drivers_file keep rows where race is in their list of races they will compete in
+        season_drivers = drivers_and_races
+        season_drivers['inRace'] = 0
+        for d in range(0, len(season_drivers)):
+            e = season_drivers.iloc[d]
+            if str(race) in season_drivers.iloc[d].Rounds:
+                season_drivers.iloc[d,3] = 1
+        season_drivers = season_drivers[season_drivers.inRace == 1]
+
         race_drivers_elo = season_drivers.merge(elo_ratings_table, on='driver', how='left')
-        race_drivers_elo = race_drivers_elo.fillna(1400)
+        race_drivers_elo = race_drivers_elo.fillna(1500)
         drivers_who_won = []
         # simulate that actual race
         for subRace in range(0, len(race_drivers_elo)):
@@ -50,13 +64,13 @@ for season in range(1, 1001):
             # go through every driver left in the race and keep track of their Elo ratings
             for i in range(0, len(race_drivers_elo)):
                 current_driver = race_drivers_elo.iloc[i, 0]
-                current_q = 10**((race_drivers_elo.iloc[i, 2])/400)
+                current_q = 10**((race_drivers_elo.iloc[i, 4])/400)
                 sum_opponents_q = 0
 
                 # go through all of the other drivers that aren't the above driver and keep track of their Elo ratings
                 for j in range(0, len(race_drivers_elo)):
                     if race_drivers_elo.iloc[j, 0] != current_driver:
-                        opponent_q = 10**((race_drivers_elo.iloc[j, 2])/400)
+                        opponent_q = 10**((race_drivers_elo.iloc[j, 4])/400)
                         sum_opponents_q += opponent_q
                 r_temp = pd.DataFrame(data={'driver': [current_driver],
                                             'winprob': [(current_q/(current_q+sum_opponents_q))]})
@@ -95,6 +109,7 @@ for season in range(1, 1001):
         update_elo = update_elo[update_elo.driver_x != update_elo.driver_y]
         update_elo = update_elo.merge(elo_ratings_table, how='left', left_on='driver_x', right_on='driver')
         update_elo = update_elo.merge(elo_ratings_table, how='left', left_on='driver_y', right_on='driver')
+        update_elo = update_elo.fillna(1500)
         update_elo['xWin'] = np.where(update_elo['fin_x']<update_elo['fin_y'], 1,0)
         update_elo['xExpectedWin'] = (1/(1+10**((update_elo['EloRating_y']-update_elo['EloRating_x'])/400)))
         update_elo.columns = ['season_x', 'race_x', 'driver_x', 'fin_x', 'key', 'season_y', 'race_y', 'driver_y', 'fin_y',
@@ -106,21 +121,25 @@ for season in range(1, 1001):
         update_elo = update_elo.reset_index(level=['driver_x'])
         update_elo.columns = ['driver', 'oldRating', 'actualScore', 'expectedScore', 'EloRating']
         update_elo = update_elo[['driver', 'EloRating']]
-        elo_ratings_table = update_elo
-        #print(update_elo)
+        # update elo ratings of drivers who were in that race
+        elo_ratings_table = elo_ratings_table.merge(update_elo, how='left', on='driver')
+        elo_ratings_table['EloRating_x'] = [x if pd.isna(y) else y for x,y in zip(elo_ratings_table['EloRating_x'], elo_ratings_table['EloRating_y'])]
+        elo_ratings_table.columns = ['driver', 'EloRating', 'EloRatingX']
+        elo_ratings_table = elo_ratings_table[['driver', 'EloRating']]
 
     print(datetime.now() - startTime)
 
     season_results = race_results[race_results.fin != 0]
     season_results = season_results.merge(points_table, on='fin', how='left')
-    season_results['points'] = [x*2 if y == 7 or y == 13 else x for x,y in zip(season_results['points'], season_results['race'])]
+    season_results['points'] = [x*2 if y == 8 or y == 14 else x for x,y in zip(season_results['points'], season_results['race'])]
     season_results = season_results.groupby('driver').agg({'points': 'sum'})
-    season_results = season_results.merge(season_drivers, on='driver', how='left')
+    print(season_results)
+    season_results = season_results.merge(drivers_and_races, on='driver', how='left')
     season_results['totalPoints'] = season_results['points_x']+season_results['points_y']
     season_results['chamPos'] = season_results['totalPoints'].rank(method='first', ascending=False)
     season_results['season'] = season
     season_results = season_results[['driver', 'totalPoints', 'chamPos', 'season']]
     final_results = final_results.append(season_results)
 
-final_results.to_csv("6_8_2020_champ.csv")
+final_results.to_csv("6_15_2020_champ.csv")
 
