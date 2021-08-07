f8223b7ed6a82dfe42f38b9fbc537d38bc7b56d6
DrewBennison <dhb5140@psu.edu>
2021-08-07 17:19:02 -0400

    updated for Nashville

--- scripts/production/sector_heatmap.R
+++ scripts/production/sector_heatmap.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(here)
 library(lubridate)
 
-source("C:/Users/drewb/Desktop/thesingleseater/scripts/thesingleseater_theme.R")
+source("C:/Users/drewb/Desktop/Projects/thesingleseater/scripts/production/thesingleseater_theme.R")
 
 #Read in data and driver results for that session
 data<-fread("C:/Users/drewb/Downloads/indycar-topsectiontimes-p2.csv")
@@ -32,13 +32,13 @@ data$percRelativeSection<-((data$Speed-data$avgSecSpeed)/data$avgSecSpeed)*100
 setkey(data, "Driver")
 #data<-merge(data, drivers, all.x = TRUE)
 
-sectors<-c("SF to T1", "T1 to SS1", "SS1 to T2", "T2 to BS","BS to T3", "T3 to SS2", "SS2 to T4", "T4 to FS", "FS to SF", "Full Lap")
+sectors<-c("SF to I1","I1 to I2", "I2 to I3", "I3 to I4", "I4 to I5","I5 to I6", "I6 to I7", "I7 to I8", "I8 to I9", "I9 to I10", "I10 to I11", "I11 to I12", "I12 to I13", "I13 to I14", "I14 to I15", "I15 to I16", "I16 to SF", "Full Lap")
 
 #Tile plot. Adjust limits depending on the number of sectors at that track
 plot<-ggplot(data=data, aes(x=Section, y=reorder(Driver, -Car))) + 
   geom_tile(aes(fill=percRelativeSection)) + 
-  scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, limits=c(-3,3)) +
-  labs(fill = "% Faster\nThan Average\nSector Speed", title="2020 Indianapolis 500 Practice 2",
+  scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, limits=c(-10,10)) +
+  labs(fill = "% Faster\nThan Average\nSector Speed", title="2021 Nashville GP Practice 2",
        subtitle = "Drivers' top sector times: Above average (purple) means faster",
        caption = "thesingleseater.com") + ylab("Driver") + xlab("Sector") + 
   scale_x_discrete(limits=sectors) +
@@ -47,14 +47,14 @@ plot<-ggplot(data=data, aes(x=Section, y=reorder(Driver, -Car))) +
 
 plot
 
-
+ggsave("C:/Users/drewb/Desktop/plot2.png", height = 8, width = 10.5)
 
 #  Natural language output of who was quickest in each sector. ############################
 for (i in sectors) {
   d <- data %>%
     filter(Section==i) %>% 
     top_n(1, Speed)
-  message(paste0(d[,1]," was quickest through ",d[,2], " and was ", round(d[,10],2), "% faster than the average car."))
+  message(paste0(d[,4]," was quickest through ",d[,1], " and was ", round(d[,10],2), "% faster than the average car."))
 }
 
 #  Get the "ultimate lap" in seconds:  ###################################################
@@ -75,6 +75,9 @@ ggplot(top_times, aes(x=Section, y=clean)) + geom_point()
 
 #  R^2 between each sector and the full lap time  ###################################
 
+
+data <- data %>% filter(!is.na(percRelativeSection))
+
 non_lap<-data %>% 
   filter(Section!="Full Lap")
 lap<-data %>%
@@ -85,6 +88,7 @@ merged<-non_lap %>%
   left_join(lap, by="Driver")
 
 for (i in sectors) {
+  print (i)
   test<-merged %>% 
     filter(Section==i) %>% 
     select(Speed.x, Speed.y) 
